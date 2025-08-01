a19cd6c5eb3144aa492a8144171aba0af0ac12e9
vikaskumar112202 <vikaskuma112202@gmail.com>
2025-08-01 22:43:35 +0530

    guess the number

---
+++ learning/script.js
@@ -0,0 +1,23 @@
+for(let i=1; i<=10; i++){
+   // console.log(`Outer loop value : ${i}`);
+    for(let j=1; j<=10; j++){
+      //  console.log(i +'*' + j +'=' + i*j);
+        
+    }    
+}
+let array=['flash','tom','jerry'];
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    //console.log(element);
+    
+    
+}
+for (let index = 1; index <= 20; index++) {
+    if(index==5){
+        console.log(`five detected`);
+        continue
+        
+    } 
+console.log(`The value of index is ${index}`);
+    
+}
